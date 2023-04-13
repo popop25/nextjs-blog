568cbc32d59728c2de5698d52000196725df0c18
Park JeongHo <popramel25@gmail.com>
2023-04-13 21:18:25 +0900

    func on

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,15 @@
 import Link from 'next/link';
 
+function bounceball(){
+  location.href = index.html;
+  console.log(location.href);
+}
 export default function FirstPost() {
   return (
     <>
       <h1>pikachu is yellow</h1>
       <h2>
         <Link href="/">Back to home</Link>
-        console.log(location.href);
-        location.href = index.html';
-
       </h2>
     </>
   );
